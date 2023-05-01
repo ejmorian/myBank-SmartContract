53d5e2ea743a45570a86a934053b9227ccc9f05c
ejmorian <ejmorian@gmail.com>
2023-05-01 22:02:21 +0800

    add deposit, withdrawAll, and security measures

--- contracts/MyBank.sol
+++ contracts/MyBank.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-//can recieve ERC20 tokens
-//can recieve ERC721 tokens
-
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MyBank {
     address private immutable owner;
+    mapping(address => uint) balance;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Permission Denied");
@@ -18,13 +16,54 @@ contract MyBank {
         owner = msg.sender;
     }
 
-    receive() external payable {}
+    function approve(
+        address _token,
+        uint256 _amount
+    ) internal returns (bool success) {
+        return IERC20(_token).approve(address(this), _amount);
+    }
 
-    fallback() external payable {}
+    function deposit(address _token, uint256 _amount) external {
+        require(approve(_token, _amount), "Approval Request Failed.");
 
-    function withdraw(address _erc20, uint256 _amount) public onlyOwner {
+        require(
+            IERC20(_token).transferFrom(msg.sender, address(this), _amount),
+            "Deposit Transfer Failed."
+        );
+
+        balance[_token] += _amount;
+    }
+
+    function withdraw(address _token, uint256 _amount) external onlyOwner {
         address _to = msg.sender;
+        require(
+            IERC20(_token).balanceOf(address(this)) >= _amount,
+            "Insufficient Balance."
+        );
+        require(
+            IERC20(_token).transfer(_to, _amount),
+            "Withdraw Transfer Failed."
+        );
+        balance[_token] -= _amount;
+    }
 
-        IERC20(_erc20).transfer(_to, _amount);
+    function withdrawAll(address _token) external onlyOwner {
+        address _to = msg.sender;
+        uint256 _amount = balance[_token];
+
+        require(
+            IERC20(_token).balanceOf(address(this)) >= _amount,
+            "Insufficient Balance."
+        );
+        require(
+            IERC20(_token).transfer(_to, _amount),
+            "Withdraw Transfer Failed."
+        );
+
+        balance[_token] = 0;
+    }
+
+    function checkBalance(address _token) external view returns (uint256) {
+        return balance[_token];
     }
 }
